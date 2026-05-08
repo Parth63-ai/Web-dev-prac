f4395f497b5210250761347d88299266f2718b26
Kumar Partha <kumarpartha726@gmail.com>
2026-05-08 11:00:56 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,45 +1,22 @@
-// Q-1
-// import { useState } from "react";
 
-// function App() {
-//   const [liked, setLiked] = useState(false);
+import Like from "./components/Like"
+import Fetch from "./components/Fetch"
+import React from 'react'
 
-//   return (
-//     <>
-//       <h1>{liked ? "❤️" : "🤍"}</h1>
-//       <button onClick={() => setLiked(!liked)}>Click it</button>
-//     </>
-//   );
-// }
+function App() {
+  return (
+    <div>
+      <Like/>
+      <Fetch/>
+    </div>
+  )
+}
 
-// export default App;
+export default App
 
-// Q-2
 
-// import { useEffect, useState } from "react";
 
-// function App() {
-//   const [users, setUsers] = useState([]);
 
-//   useEffect(() => {
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//       .then(res => res.json())
-//       .then(data => setUsers(data));
-//   }, []);
-   
-//   return (
-//     <>
-//       {users.map(user => (
-//         <div key={user.id}>
-//           <p>{user.name}</p>
-//           <p>{user.website}</p>
-//         </div>
-//       ))}
-//     </>
-//   );
-// }
-
-// export default App;
 
 
 
